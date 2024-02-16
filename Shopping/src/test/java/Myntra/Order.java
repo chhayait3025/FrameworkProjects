83ce6590ac02908dc33c3009ce78886c9207977d
chhayait3025 <chhaya.singh0902@gmail.com>
2024-02-16 15:07:59 -0500

    first commit

---
+++ Shopping/src/test/java/Myntra/Order.java
@@ -0,0 +1,26 @@
+package Myntra;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Order {
+
+    @Test
+   public void zohoTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.zoho.com/index1.html");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
+        driver.findElement(By.xpath("//input[@id='login_id']")).sendKeys("chhaya.singh0902@gmail.com");
+        driver.findElement(By.xpath("//button[@id='nextbtn']")).click();
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password@#$%6@12345");
+        driver.findElement(By.xpath("//button[@id='nextbtn']")).click();
+        driver.quit();
+    }
+
+}
