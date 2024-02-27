ae71681ff923490528ab1720f2caf23a5383c463
chhayait3025 <chhaya.singh0902@gmail.com>
2024-02-27 18:22:56 -0500

    added edge driver dependency

--- Shopping/src/test/java/Myntra/Order.java
+++ Shopping/src/test/java/Myntra/Order.java
@@ -3,6 +3,8 @@ package Myntra;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,7 +13,9 @@ public class Order {
 
     @Test
    public void zohoTest(){
-        WebDriver driver = new ChromeDriver();
+        EdgeOptions options=new EdgeOptions();
+        options.addArguments("--headless");
+        WebDriver driver=new EdgeDriver(options);
         driver.get("https://www.zoho.com/index1.html");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
